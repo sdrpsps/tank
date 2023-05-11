eb2c8cd213322be52440a1e5a7a35f9d5e0dc8aa
sdrpsps <z826700@gmail.com>
2023-05-11 16:56:37 +0800

    游戏启动页

---
+++ src/app.ts
@@ -0,0 +1,43 @@
+import config from '../config/index';
+import BossCanvas from './canvas/BossCanvas';
+import BulletCanvas from './canvas/BulletCanvas';
+import PlayerCanvas from './canvas/PlayerCanvas';
+import SteelCanvas from './canvas/SteelCanvas';
+import StrawCanvas from './canvas/StrawCanvas';
+import TankCanvas from './canvas/TankCanvas';
+import WallCanvas from './canvas/WallCanvas';
+import WaterCanvas from './canvas/WaterCanvas';
+import { promises } from './service/images';
+import './style.scss';
+
+const app = document.querySelector<HTMLDivElement>('#app')!;
+
+app.style.width = config.canvas.width + 'px';
+app.style.height = config.canvas.height + 'px';
+
+export default {
+  isStart: false,
+  bootstrap() {
+    app.addEventListener('click', () => this.start());
+  },
+
+  async start() {
+    if (this.isStart) return;
+    this.isStart = true;
+
+    app.style.backgroundImage = 'none';
+
+    await Promise.all(promises);
+
+    StrawCanvas.render();
+    WallCanvas.render();
+    WaterCanvas.render();
+    SteelCanvas.render();
+    TankCanvas.render();
+    BulletCanvas.render();
+    BossCanvas.render();
+    PlayerCanvas.render();
+  },
+
+  end() {},
+};
--- src/main.ts
+++ src/main.ts
@@ -1,30 +1,3 @@
-import config from '../config/index';
-import BossCanvas from './canvas/BossCanvas';
-import BulletCanvas from './canvas/BulletCanvas';
-import PlayerCanvas from './canvas/PlayerCanvas';
-import SteelCanvas from './canvas/SteelCanvas';
-import StrawCanvas from './canvas/StrawCanvas';
-import TankCanvas from './canvas/TankCanvas';
-import WallCanvas from './canvas/WallCanvas';
-import WaterCanvas from './canvas/WaterCanvas';
-import { promises } from './service/images';
-import './style.scss';
+import app from './app';
 
-const app = document.querySelector<HTMLDivElement>('#app')!;
-
-app.style.width = config.canvas.width + 'px';
-app.style.height = config.canvas.height + 'px';
-
-async function bootstrap() {
-  await Promise.all(promises);
-  StrawCanvas.render();
-  WallCanvas.render();
-  WaterCanvas.render();
-  SteelCanvas.render();
-  TankCanvas.render();
-  BulletCanvas.render();
-  BossCanvas.render();
-  PlayerCanvas.render();
-}
-
-bootstrap();
+app.bootstrap();
